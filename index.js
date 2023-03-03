243599b50c7b8897a595f4cc2195c71fbb6f0489
Utilizator <Stanislavbox24@gmail.com>
2023-03-03 22:40:58 +0200

    Задачки

--- index.js
+++ index.js
@@ -290,3 +290,33 @@
 //     }
 
 //     console.log(newArr);
+
+
+const arr = [
+      { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+      { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+      { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+      { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+      { name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
+      { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
+      { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
+      { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
+      { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
+      { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
+      { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
+      { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
+      { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 }
+      ];
+      
+      // - Получить массив ученых, родившихся в 19 веке.
+      // - Найти общую сумму лет, сколько прожили все ученые.
+      // - Отсортировать ученых по алфавиту.
+      // - Отсортировать ученых по количеству прожитых лет.
+      // - Удалить из массива ученых, родившихся в 15, 16 или 17 веках.
+      // - Найти ученого, который родился позже всех.
+      // - Найти год рождения Albert Einstein.
+      // - Найти ученых, фамилия которых начинается на букву "С".
+      // - Удалить из массива всех ученых, имя которых начинается на букву "A".
+      // - Найти ученого, который прожил больше всех и ученого, который меньше.
+      // - Найти ученых, у которых совпадают первые буквы имени и фамилии.
+      // - Узнать, все ли ученые работали в 19 веке.
