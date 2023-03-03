6c9f78da6d3a4a64e3ae918301509a210b25ba87
Роман Харабара <117296212+KharabaraRoman@users.noreply.github.com>
2023-03-03 20:46:30 +0200

    Roman

--- index.js
+++ index.js
@@ -195,3 +195,27 @@
 // };
 
 // console.log(calcTotalPrice(stones, "Изумруд"));
+
+
+//  *? У нас є об'єкт, у якому зберігаються зарплати
+//  *? нашої команди
+//  *? Напишіть код для підсумовування всіх зарплат і
+//  *? збережіть його результат у змінній sum.
+//  *? Якщо об'єкт salaries порожній, то результат має бути 0
+//  */
+
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+
+// function salary(object) {
+//     let sum = 0;
+//     const array = Object.values(object);
+//     for (const value of array) {
+//         sum += value;
+//     }
+//     return sum;
+// }
+// console.log(salary(salaries));
