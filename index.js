19ed3954e446096b7c46c69b34ec303f7937be94
Utilizator <Stanislavbox24@gmail.com>
2023-03-17 21:03:38 +0200

    Stas

--- index.js
+++ index.js
@@ -292,21 +292,21 @@
 //     console.log(newArr);
 
 
-const arr = [
-      { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
-      { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
-      { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
-      { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
-      { name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
-      { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
-      { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
-      { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
-      { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
-      { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
-      { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
-      { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
-      { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 }
-      ];
+// const arr = [
+//       { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+//       { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+//       { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+//       { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+//       { name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
+//       { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
+//       { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
+//       { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
+//       { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
+//       { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
+//       { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
+//       { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
+//       { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 }
+//       ];
       
       // - Получить массив ученых, родившихся в 19 веке.
       // - Найти общую сумму лет, сколько прожили все ученые.
@@ -335,17 +335,49 @@ const arr = [
 //Додай метод getInfo(), який повертає рядок:
 //`Користувачеві ім'я кількість років і в нього кількість публікацій.`
 
-const User = function ({ userName, age, numbersOfPost }) {
-    this.userName = userName;
-    this.age = age;
-    this.numbersOfPost = numbersOfPost;
-    this.getInfo = function (){
- return (`Користувачеві ${userName} ${age} років і в нього ${numbersOfPost} публікацій.`);
-    } 
+// const User = function ({ userName, age, numbersOfPost }) {
+//     this.userName = userName;
+//     this.age = age;
+//     this.numbersOfPost = numbersOfPost;
+//     this.getInfo = function (){
+//  return (`Користувачеві ${userName} ${age} років і в нього ${numbersOfPost} публікацій.`);
+//     } 
        
-}
+// }
 
-const newUser = new User({ userName: "Anna", age: 22, numbersOfPost: 5 });
-console.log(newUser);
-console.log(newUser.getInfo());
-console.log(newUser.userName);
+// const newUser = new User({ userName: "Anna", age: 22, numbersOfPost: 5 });+
+// console.log(newUser);
+// console.log(newUser.getInfo());
+// console.log(newUser.userName);
+
+//TODO:=============================================
+//Напиши функцію конструктор Storage який створює об'єкти
+//Для управління складом товарів.
+//При виклику отримуватиме один агрумент - початковий масив товарів,
+//і записувати їх у властивість items.
+//Додай методи класу:
+//getItems() - повертайте масив товарів
+//addItems(item) - отримує новий товар та додає його до поточних
+//removeItem(item) - отримує товар і, якщо він є, видаляє його з поточних
+
+
+// const Storage = function(arr) {
+//     this.items = arr;
+//     this.getItems = function(){
+//         return this.items;
+//     }
+//     this.addItems = function(item){
+//        this.items.push(item);
+//     }
+
+// }
+// Storage.prototype.removeItem = function(item){
+//    if(this.items.includes(item)){
+//     this.items.splice(this.items.indexOf(item), 1);
+//    } 
+// }
+// const newStorage = new Storage(['one', 'two', 'three']);
+// console.log(newStorage.getItems());
+// console.log(newStorage.addItems('qwe'));
+// console.log(newStorage.removeItem('two'));
+// console.log(newStorage.getItems());
